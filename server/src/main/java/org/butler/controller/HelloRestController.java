02d0a51ea2a3d67527d5943761a0190001803425
Shilov Ilya <ShilovIlya@gmail.com>
2018-07-08 07:01:44 +0300

    add client and server

---
+++ server/src/main/java/org/butler/controller/HelloRestController.java
@@ -0,0 +1,13 @@
+package org.butler.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloRestController {
+
+    @RequestMapping("/api/hello")
+    public String getHello() {
+        return "Hello form butler server side!!! More fun!";
+    }
+}
